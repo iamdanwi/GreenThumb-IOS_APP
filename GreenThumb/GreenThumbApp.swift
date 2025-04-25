e1c01c6361353b2ddeb27a06a2919c50defb4d79
iamdanwi <danwichoudhary@gmail.com>
2025-04-25 17:25:36 +0530

    Initial Commit

---
+++ GreenThumb/GreenThumbApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenThumbApp.swift
+//  GreenThumb
+//
+//  Created by Dainwi on 25/04/25.
+//
+
+import SwiftUI
+
+@main
+struct GreenThumbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GreenThumbTests/GreenThumbTests.swift
@@ -0,0 +1,17 @@
+//
+//  GreenThumbTests.swift
+//  GreenThumbTests
+//
+//  Created by Dainwi on 25/04/25.
+//
+
+import Testing
+@testable import GreenThumb
+
+struct GreenThumbTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
